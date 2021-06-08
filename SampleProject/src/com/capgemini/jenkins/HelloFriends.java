ad85c1d59ac8371e31b7416848189741168c0d2b
Roshma-JA17 <84120858+Roshma-JA17@users.noreply.github.com>
2021-06-08 09:54:13 +0530

    Add files via upload

---
+++ SampleProject/src/com/capgemini/jenkins/HelloFriends.java
@@ -0,0 +1,11 @@
+package com.capgemini.jenkins;
+
+public class HelloFriends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heloo Friend");
+
+	}
+
+}
